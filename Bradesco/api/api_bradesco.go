87b6d73943df9295a5fa30d48f1325e643168885
peder1981 <110640912+peder1981@users.noreply.github.com>
2025-01-08 11:43:17 -0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Changes to be committed:
    	modified:   Bradesco/api/api_bradesco.go

--- Bradesco/api/api_bradesco.go
+++ Bradesco/api/api_bradesco.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strings"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -61,6 +62,10 @@ type Token struct {
 	Token        string
 }
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func createTable(db *sql.DB) {
 	_, err := db.Exec(`
 		CREATE TABLE IF NOT EXISTS pix_transfers (
@@ -97,7 +102,22 @@ func createTable(db *sql.DB) {
 	}
 }
 
-func insertData(db *sql.DB, data PixTransferRequest) {
+func getRandomStatus() string {
+	if rand.Float32() < 0.5 {
+		return "CONCLUIDO"
+	}
+	return "EM_PROCESSAMENTO"
+}
+
+func getMotivo(status string) string {
+	if status == "CONCLUIDO" {
+		return "Transação realizada com sucesso"
+	}
+	return "Transação em processamento"
+}
+
+func insertData(db *sql.DB, data PixTransferRequest) string {
+	status := getRandomStatus()
 	_, err := db.Exec(`
 		INSERT INTO pix_transfers (
 			pagador_chave_pix,
@@ -125,18 +145,14 @@ func insertData(db *sql.DB, data PixTransferRequest) {
 		data.Valor,
 		data.Descricao,
 		time.Now().Format("2006-01-02T15:04:05.999Z"),
-		func() string {
-			if rand.Float32() < 0.5 {
-				return "CONCLUIDO"
-			}
-			return "EM_PROCESSAMENTO"
-		}(),
+		status,
 		"0.00",
-		"Transação realizada com sucesso",
+		getMotivo(status),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return status
 }
 
 func handlePixTransfer(w http.ResponseWriter, r *http.Request) {
@@ -155,7 +171,7 @@ func handlePixTransfer(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	createTable(db)
-	insertData(db, transferRequest)
+	status := insertData(db, transferRequest)
 
 	uuid, err := generateUUIDV4X()
 
@@ -182,24 +198,156 @@ func handlePixTransfer(w http.ResponseWriter, r *http.Request) {
 			ChavePix:       transferRequest.Recebedor.ChavePix,
 			NomeFavorecido: transferRequest.Recebedor.NomeFavorecido,
 		},
-		Valor: transferRequest.Valor,
-		//E2e:         "E60746948202211301715L2856oOXfn8",
+		Valor:       transferRequest.Valor,
 		E2e:         uuid,
 		IdTransacao: transferRequest.IdTransacao,
 		Descricao:   transferRequest.Descricao,
 		DataCriacao: time.Now().Format("2006-01-02T15:04:05.999Z"),
-		Status: func() string {
-			if rand.Float32() < 0.5 {
-				return "CONCLUIDO"
-			}
-			return "EM_PROCESSAMENTO"
-		}(),
+		Status:      status,
 		ValorTarifa: "0.00",
-		Motivo:      "Transação realizada com sucesso",
+		Motivo:      getMotivo(status),
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(transferResponse)
+}
+
+func handleGetPixTransfer(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Extract path parameters
+	path := r.URL.Path
+	parts := strings.Split(path, "/")
+	if len(parts) != 7 {
+		http.Error(w, "Invalid URL format", http.StatusBadRequest)
+		return
+	}
+
+	idTransacao := parts[5]
+	e2e := parts[6]
+
+	if idTransacao == "" && e2e == "" {
+		http.Error(w, "idTransacao or e2e parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "../interface/pix.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	var transfer PixTransferResponse
+	var rows *sql.Rows
+
+	if idTransacao != "" {
+		rows, err = db.Query(`
+            SELECT 
+                pagador_chave_pix, pagador_agencia, pagador_conta,
+                recebedor_cpf_cnpj, recebedor_tipo_chave, recebedor_chave_pix, recebedor_nome_favorecido,
+                valor, descricao, data_criacao, status, valor_tarifa, motivo
+            FROM pix_transfers 
+            WHERE id_transacao = ?
+            ORDER BY id DESC LIMIT 1`, idTransacao)
+	} else {
+		rows, err = db.Query(`
+            SELECT 
+                pagador_chave_pix, pagador_agencia, pagador_conta,
+                recebedor_cpf_cnpj, recebedor_tipo_chave, recebedor_chave_pix, recebedor_nome_favorecido,
+                valor, descricao, data_criacao, status, valor_tarifa, motivo
+            FROM pix_transfers 
+            WHERE e2e = ?
+            ORDER BY id DESC LIMIT 1`, e2e)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		http.Error(w, "Transaction not found", http.StatusNotFound)
+		return
+	}
+
+	var (
+		pagadorChavePix, pagadorAgencia, pagadorConta                                    string
+		recebedorCpfCnpj, recebedorTipoChave, recebedorChavePix, recebedorNomeFavorecido string
+		valor, descricao, dataCriacao, status, valorTarifa, motivo                       string
+	)
+
+	err = rows.Scan(
+		&pagadorChavePix, &pagadorAgencia, &pagadorConta,
+		&recebedorCpfCnpj, &recebedorTipoChave, &recebedorChavePix, &recebedorNomeFavorecido,
+		&valor, &descricao, &dataCriacao, &status, &valorTarifa, &motivo,
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Se o status for "EM_PROCESSAMENTO", atualizamos para "CONCLUIDO"
+	if status == "EM_PROCESSAMENTO" {
+		status = "CONCLUIDO"
+		motivo = "Transação realizada com sucesso"
+
+		// Atualiza o status no banco de dados
+		_, err = db.Exec(`
+            UPDATE pix_transfers 
+            SET status = ?, motivo = ? 
+            WHERE id = (
+                SELECT id FROM pix_transfers 
+                WHERE id_transacao = ? OR e2e = ?
+                ORDER BY id DESC LIMIT 1
+            )`,
+			status, motivo, idTransacao, e2e)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	transfer = PixTransferResponse{
+		Pagador: struct {
+			CpfCnpj   string `json:"cpfCnpj"`
+			Agencia   string `json:"agencia"`
+			Conta     string `json:"conta"`
+			TipoConta string `json:"tipoConta"`
+		}{
+			CpfCnpj:   pagadorChavePix,
+			Agencia:   pagadorAgencia,
+			Conta:     pagadorConta,
+			TipoConta: "CONTA_CORRENTE",
+		},
+		Recebedor: struct {
+			CpfCnpj        string `json:"cpfCnpj"`
+			TipoChave      string `json:"tipoChave"`
+			ChavePix       string `json:"chavePix"`
+			NomeFavorecido string `json:"nomeFavorecido"`
+		}{
+			CpfCnpj:        recebedorCpfCnpj,
+			TipoChave:      recebedorTipoChave,
+			ChavePix:       recebedorChavePix,
+			NomeFavorecido: recebedorNomeFavorecido,
+		},
+		Valor:       valor,
+		E2e:         e2e,
+		IdTransacao: idTransacao,
+		Descricao:   descricao,
+		DataCriacao: dataCriacao,
+		Status:      status,
+		ValorTarifa: valorTarifa,
+		Motivo:      motivo,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(transfer)
 }
 
 func handleOAuthToken(w http.ResponseWriter, r *http.Request) {
@@ -253,6 +401,7 @@ func main() {
 	fmt.Println("Iniciando servidor...")
 
 	http.HandleFunc("/v1/spi/solicitar-transferencia", handlePixTransfer)
+	http.HandleFunc("/v1/spi/consulta/transferencia/", handleGetPixTransfer)
 	http.HandleFunc("/oauth/token", handleOAuthToken)
 
 	// Canal para sincronização de erros
